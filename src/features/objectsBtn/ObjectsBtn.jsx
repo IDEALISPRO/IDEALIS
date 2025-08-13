25cd0c8e3df4133526130ea611fd86a56234c2f6
Islam-Nuraliev <islamnuraliev2020@gmail.com>
2025-08-14 01:13:49 +0300

    add icon mui

--- src/features/objectsBtn/ObjectsBtn.jsx
+++ src/features/objectsBtn/ObjectsBtn.jsx
@@ -1,15 +1,14 @@
 import './objectsBtn.scss'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useNavigate } from 'react-router-dom';
 
 export const ObjectsBtn = () => {
     return (
-        <button className='objBtn'>
-            <div className='objBtn__btn'>
-                <svg className='objBtn__btn__svg' xmlns="http://www.w3.org/2000/svg" width="67" height="63" viewBox="0 0 67 63" fill="none">
-                    <path d="M35.834 3.4834L63.834 31.4834L35.834 59.4834M59.9451 31.4834L3.16732 31.4834" stroke="#163659" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+        <button className='objectsMoreBtn'>
+            <div className='objectsMoreBtn__btn'>
+                <ArrowForwardIcon className='objectsMoreBtn__btn__icon' />
             </div>
-            <h2 className='objBtn__text'>Посмотреть все</h2>
+            <h2 className='objectsMoreBtn__text'>Посмотреть все</h2>
         </button>
     )
 }
--- src/features/objectsCard/ObjectsCard.jsx
+++ src/features/objectsCard/ObjectsCard.jsx
@@ -1,6 +1,8 @@
 import './objectsCard.scss';
 import loca from '../../shared/icons/location.svg';
 import phone from '../../shared/icons/phone.svg';
+import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import { useState } from 'react';
 
 export const ObjectsCard = ({ img, title, location, description, price }) => {
@@ -25,11 +27,12 @@ export const ObjectsCard = ({ img, title, location, description, price }) => {
 
             <div className='objCard__buttons'>
                 <button className='objCard__buttons__more'>Подробнее</button>
-                <button className='objCard__buttons__phone'><img className='objCard__buttons__phone__mark' src={phone} alt="" /></button>
-                <button onClick={HandleLike} className={`objCard__buttons__like ${liked ? `red` : ''}`}>
-                    <svg className={`objCard__buttons__like__mark ${liked ? `red` : ''}`} stroke="grey" strokeWidth="2px" width="24" height="24" viewBox="9.5 -2 11 30" fill="white" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.7 1.96924C4.4475 1.96924 1 5.41674 1 9.66924C1 17.3692 10.1 24.3692 15 25.9974C19.9 24.3692 29 17.3692 29 9.66924C29 5.41674 25.5525 1.96924 21.3 1.96924C18.696 1.96924 16.393 3.26214 15 5.24104C14.2899 4.22981 13.3466 3.40452 12.25 2.83502C11.1533 2.26551 9.93568 1.96854 8.7 1.96924Z" />
-                    </svg></button>
+                <button className='objCard__buttons__phone'>
+                    <LocalPhoneIcon className='objCard__buttons__phone__icon' />
+                </button>
+                <button onClick={HandleLike} className={"objCard__buttons__like"}>
+                    <FavoriteSharpIcon className={`objCard__buttons__like__icon ${liked ? `red` : ''}`} />
+                </button>
             </div>
         </div>
     );
