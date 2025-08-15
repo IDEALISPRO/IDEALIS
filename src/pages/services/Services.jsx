ad6468c2ae2b2f4675d60d54a737a803059723d8
Amange1di <amangeldi2702006@gmail.com>
2025-08-15 17:07:03 +0600

    fix

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,8 +1,10 @@
 import { ServicesWidget } from "../../widgets";
+import { Banner } from "../../features";
 
 export const Services = () => {
     return (
         <div>
+            <Banner title={'Услуги'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'} />
 
             <ServicesWidget />
 
