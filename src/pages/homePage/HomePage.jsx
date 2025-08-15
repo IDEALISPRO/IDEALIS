3a5c1d092220b82576272cac7a5a82f772e7573c
Amange1di <amangeldi2702006@gmail.com>
2025-08-15 17:19:02 +0600

    git

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "../../widgets";
 import { FilterWidget } from "../../widgets/FilterWidget/FilterWidget";
-import { Banner } from "../../features";
+import { Banner, BannerPictures } from "../../features";
 
 export const HomePage = () => {
     return (
         <div className="container">
             <Banner title={'IDEALIS'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'} />
+            <BannerPictures />
             <Navigate />
 
             <FilterWidget />
