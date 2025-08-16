88b18afc6bd5cbf955541bd422af8c782b0cc30e
Islam-Nuraliev <islamnuraliev2020@gmail.com>
2025-08-16 16:07:59 +0300

    slice

--- src/features/hotOffersCard/HotOfferCard.jsx
+++ src/features/hotOffersCard/HotOfferCard.jsx
@@ -2,11 +2,19 @@ import './hotOffersCard.scss'
 
 export const HotOfferCard = ({image, description, isActive, onClick, descriptionSecond}) => {
 
+  const TextWidth = (text) => {
+    if (text.length > 58) {
+      return text.slice(0, 58) + '...';
+    }else{
+      return text
+    }
+  }
+
   return (
     <div onClick={onClick} className={`hotOffersCard ${isActive ? 'active' : ''}`}>
             <img className='hotOffersCard__img' src={image} alt="image" />
             
-            <h3 className='hotOffersCard__description'>{description}</h3>
+            <h3 className='hotOffersCard__description'>{TextWidth(description)}</h3>
             <h3 className={`hotOffersCard__description adaptive ${isActive ? '' : 'none'}`}>{descriptionSecond}</h3>
     </div>
   )
