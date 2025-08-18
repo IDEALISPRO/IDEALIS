12c2848dbc1b122883924e2ed4da0f305f8d7aa3
Your Name <emirmarash@gmail.com>
2025-08-18 16:02:25 +0600

    created a widget EventsSection and updated adaptation

--- src/widgets/NewsEvent/EventsSection.jsx
+++ src/widgets/NewsEvent/EventsSection.jsx
@@ -6,29 +6,45 @@ export const EventsSection = () => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    setEvents(data); // потом заменишь на API
+    setEvents(data)
   }, []);
 
   return (
     <section className="container events-section">
-      <h2 className="eventstitle">СОБЫТИЯ И МЕРОПРИЯТИЯ IDEALIS</h2>
-      <div className="card-container">
-       {events.map((event, index) => {
-        let sizeClass = "";
+      <h2 className="eventsTitle">СОБЫТИЯ И МЕРОПРИЯТИЯ IDEALIS</h2>
+      {events[2] && (
+  <div className="cardLarge-2">
+    <img
+      className="Img"
+      src={new URL(`./${events[2].image}`, import.meta.url).href}
+      alt={events[2].description}
+    />
+    <p className="Card-description">{events[2].description}</p>
+  </div>
+)}
 
-        if (index === 0 || index === 4) sizeClass = "events-section__card--small";    // 1 и 5
-        else if (index === 1 || index === 3) sizeClass = "events-section__card--medium"; // 2 и 4
-        else if (index === 2) sizeClass = "events-section__card--large";               // 3 центр
+<div className="card-container">
+  {events.map((event, index) => {
+    let sizeClass = "";
 
-        return (
-          <div key={event.id} className={`events-section__card ${sizeClass}`}>
-            <img src={new URL(`./${event.image}`, import.meta.url).href} alt={event.description} />
-            <p>{event.description}</p>
-          </div>
-        );
-      })}
+    if (index === 0 || index === 4) sizeClass = "cardSmall";
+    else if (index === 1 || index === 3) sizeClass = "cardMedium";
+    else if (index === 2) sizeClass = "cardLarge";
 
+    return (
+      <div key={event.id} className={`events-section__card ${sizeClass}`}>
+        <img
+          className="Img"
+          src={new URL(`./${event.image}`, import.meta.url).href}
+          alt={event.description}
+        />
+        <p className="Card-description">{event.description}</p>
       </div>
+    );
+  })}
+</div>
+
+
     </section>
   );
 };
