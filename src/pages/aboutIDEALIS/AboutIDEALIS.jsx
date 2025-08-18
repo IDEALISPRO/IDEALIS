777851914a2a2d4262adb54961be240698000bf6
Amange1di <amangeldi2702006@gmail.com>
2025-08-18 17:23:34 +0600

    fix

--- src/pages/aboutIDEALIS/AboutIDEALIS.jsx
+++ src/pages/aboutIDEALIS/AboutIDEALIS.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { BannerPictures } from "../../features";
-
+import { BannerPictures ,  } from "../../features";
+import {MissionsValues}  from "../../widgets"
 export const AboutIDEALIS = () => {
   return (
     <div>
       <BannerPictures />{" "}
-      
+       <MissionsValues />
     </div>
   );
 };
