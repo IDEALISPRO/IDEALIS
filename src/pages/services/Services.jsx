831f63e8aefa46a73de5f850830b28e60334ddfb
Amange1di <amangeldi2702006@gmail.com>
2025-08-19 15:08:04 +0600

    выа

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,13 +1,13 @@
-import { ServicesWidget } from "../../widgets";
+import { ServicesWidget, MiniMenu } from "../../widgets";
 import { Banner, BannerPictures } from "../../features";
 
 export const Services = () => {
     return (
         <div>
-            <Banner title={'Услуги'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'} />
+            {/* <Banner title={'Услуги'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'} /> */}
             <BannerPictures />
-
-            <ServicesWidget />
+            <MiniMenu />
+            {/* <ServicesWidget /> */}
 
         </div>
     );
--- src/widgets/header/miniMenu/MinieMenu.jsx
+++ src/widgets/header/miniMenu/MinieMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./miniMenu.scss";
 import { GrHomeRounded } from "react-icons/gr";
 import { RiHeartLine } from "react-icons/ri";
@@ -6,46 +6,38 @@ import { CiCirclePlus } from "react-icons/ci";
 import { HiOutlineChatAlt2 } from "react-icons/hi";
 
 export const MiniMenu = () => {
-  const [stickToTop, setStickToTop] = useState(false);
-
   useEffect(() => {
+    const footer = document.querySelector('.foterAdmin');
+
     const handleScroll = () => {
       if (window.scrollY > 100) {
-        setStickToTop(true);
+        footer.classList.add('stick-top');
       } else {
-        setStickToTop(false);
+        footer.classList.remove('stick-top');
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
-    <div className={`foterAdmin ${stickToTop ? "stick-top" : ""}`}>
+    <div className="foterAdmin">
       <div className="footer-nav">
         <a href="#home" className="footer-link">
-          <span className="icon">
-            <GrHomeRounded size={30} />
-          </span>
+          <span className="icon"><GrHomeRounded size={30} /></span>
           Главная
         </a>
         <a href="#favorites" className="footer-link">
-          <span className="icon">
-            <RiHeartLine size={30} />
-          </span>
+          <span className="icon"><RiHeartLine size={30} /></span>
           Избранное
         </a>
         <a href="#submit" className="footer-link">
-          <span className="icon">
-            <CiCirclePlus size={30} />
-          </span>
+          <span className="icon"><CiCirclePlus size={30} /></span>
           Подать
         </a>
         <a href="#chat" className="footer-link">
-          <span className="icon">
-            <HiOutlineChatAlt2 size={30} />
-          </span>
+          <span className="icon"><HiOutlineChatAlt2 size={30} /></span>
           Чат
         </a>
       </div>
