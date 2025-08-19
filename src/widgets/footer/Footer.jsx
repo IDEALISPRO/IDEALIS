e9adf2d40288fea68b836684c8aea0b9da5d2a5b
umka-pumka <toktogulovumar069@gmail.com>
2025-08-19 14:42:12 +0600

    update header

--- src/widgets/footer/Footer.jsx
+++ src/widgets/footer/Footer.jsx
@@ -1,55 +1,81 @@
 import "./footer.scss";
 import { FaTelegramPlane, FaWhatsapp, FaVk, FaInstagram } from "react-icons/fa";
-import footerLogo from '../../shared/img/logogo.jpg';
-import Geeks from '../../shared/img/geeks.png';
+import footerLogo from "../../shared/img/logogo.jpg";
+import Geeks from "../../shared/img/geeks.png";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <div className="footer ">
-      <div className="sb__footer  container section__padding">
+    <div className="footer">
+      <div className="sb__footer container section__padding">
         <div className="sb__footer-links">
-          
           <div className="sb__footer-links-div">
             <h4>Ош</h4>
             <h4>Бишкек</h4>
             <h4>Жалал Абад</h4>
           </div>
 
-          <div className="sb__footer-links-div">
+          <div className="sb__footer-links-div-text1">
             <h4 className="text_1">
-              Начните сотрудничество с IDEALIS —<br />
+              Начните сотрудничество с IDEALIS —
               найдём идеальную недвижимость для вас.
             </h4>
           </div>
 
-            <div className="sb__footer-links-div">
-        <Link to="/"><h4>Главная</h4></Link>
-        <Link to="/about"><h4>Про IDEALIS</h4></Link>
-        <Link to="/services"><h4>Услуги</h4></Link>
-        <Link to="/news"><h4>Новости</h4></Link>
-        <Link to="/contacts"><h4>Контакты</h4></Link>
-        <Link to="/favorites"><h4>Избранное</h4></Link>
-      </div> 
-   
-             <div className="sb__footer-links-div">
+          <div className="sb__footer-links-div-buttons">
+            <div className="block2">
+            <Link to="/"><h4>Главная</h4></Link>
+            <Link to="/about"><h4>Про IDEALIS</h4></Link>
+            <Link to="/services"><h4>Услуги</h4></Link>
+            </div>
+            <div className="block1">
+            <Link to="/news"><h4>Новости</h4></Link>
+            <Link to="/contacts"><h4>Контакты</h4></Link>
+            <div className="izbrannoe">
+            <Link to="/favorites"><h4>Избранное</h4></Link>
+            </div>
+            </div>
+          </div>
+
+          <div className="sb__footer-links-div-text2">
             <h4 className="text_2">
-              Самый лучший в<br />
-              Кыргызстане 2025 года<br />
-              недвижимость IDEALIS
+              Самый лучший в Кыргызстане 2025 года недвижимость IDEALIS
             </h4>
           </div>
 
-          <div className="sb__footer-links-div socials-block">
+         <div className="sb__footer-links-div-socialsmedia socials-block">
             <div className="socialmedia">
               <h4>Соц сети</h4>
               <div className="socialsmedia-icon">
-              <p><FaTelegramPlane size={24} /></p>
-              <p><FaWhatsapp size={24} /></p>
-              <p><FaVk size={24} /></p>
-              <p><FaInstagram size={24} /></p>
+                <a
+                  href="https://geeks.kg/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaTelegramPlane size={24} />
+                </a>
+                <a
+                  href="https://geeks.kg/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaWhatsapp size={24} />
+                </a>
+                <a
+                  href="https://geeks.kg/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaVk size={24} />
+                </a>
+                <a
+                  href="https://geeks.kg/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaInstagram size={24} />
+                </a>
               </div>
-              
             </div>
           </div>
         </div>
@@ -60,10 +86,15 @@ export const Footer = () => {
 
         <hr />
 
-        <a href="https://geeks.kg/" target="_blank" rel="noopener noreferrer" className="footer-madeby">
+        <a
+          href="https://geeks.kg/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-madeby"
+        >
           <img src={Geeks} alt="Geeks Logo" className="geeks-logo" />
           <span>
-            MADE BY <strong><br />GEEKS </strong>
+            MADE BY <strong><br />GEEKS</strong>
           </span>
         </a>
       </div>
