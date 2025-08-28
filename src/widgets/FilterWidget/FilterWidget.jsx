150efd4971abf4e5c4c2ceb463c71305483319d8
undef1ned7 <myizambekov.baiel9@gmail.com>
2025-08-28 14:56:10 +0600

    d

--- src/widgets/FilterWidget/FilterWidget.jsx
+++ src/widgets/FilterWidget/FilterWidget.jsx
@@ -2,22 +2,22 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
-import SearchIcon from '@mui/icons-material/Search';
-import Grid from '@mui/material/Grid';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import React, { useState } from 'react';
-import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Slider from '@mui/material/Slider';
-import Box from '@mui/material/Box';
+import SearchIcon from "@mui/icons-material/Search";
+import Grid from "@mui/material/Grid";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import React, { useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Slider from "@mui/material/Slider";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
 
 function valuetext(value) {
   return `${value}°C`;
@@ -29,10 +29,10 @@ export const FilterWidget = () => {
     alert("Иконка поиска нажата!");
   };
 
-  const [alignment, setAlignment] = React.useState('left');
-  const [age, setAge] = React.useState('');
+  const [alignment, setAlignment] = React.useState("left");
+  const [age, setAge] = React.useState("");
   const [value1, setValue1] = React.useState([100000, 1000000]);
-  const [currency, setCurrency] = React.useState('som');
+  const [currency, setCurrency] = React.useState("som");
   const [open, setOpen] = useState(false);
   const [formData, setFormData] = useState({});
 
@@ -51,7 +51,7 @@ export const FilterWidget = () => {
   const handleChangeSlider = (event, newValue, activeThumb) => {
     if (activeThumb === 0) {
       setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
-      console.log(value1)
+      console.log(value1);
     } else {
       setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
     }
@@ -69,10 +69,10 @@ export const FilterWidget = () => {
   };
 
   const handleReset = () => {
-    setAge('');
+    setAge("");
     setValue1([100000, 1000000]);
-    setCurrency('som');
-    setAlignment('left');
+    setCurrency("som");
+    setAlignment("left");
   };
 
   const handleClose = () => setOpen(false);
@@ -90,7 +90,7 @@ export const FilterWidget = () => {
                 sx={{ width: "100%" }}
                 InputLabelProps={{
                   sx: {
-                    width: "30%",      
+                    width: "30%",
                     whiteSpace: "nowrap",
                     overflow: "hidden",
                     textOverflow: "ellipsis",
@@ -110,30 +110,35 @@ export const FilterWidget = () => {
                   ),
                 }}
               />
-
             </Grid>
             <Grid item size={{ xs: 4 }}>
-              <Button fullWidth variant="contained" color="primary" type="submit">
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
                 Поиск
               </Button>
             </Grid>
-
           </Grid>
           <Box
             sx={{
-              display: { xs: "none", sm: "block" }
+              display: { xs: "none", sm: "block" },
             }}
           >
-
-
             <ToggleButtonGroup
               value={alignment}
               exclusive
               onChange={handleAlignment}
               aria-label="text alignment"
               fullWidth
-
-              sx={{ width: "100%", margin: "18px 0px", fontFamily: "Roboto Condensed", border: "1px solid #00000066" }}
+              sx={{
+                width: "100%",
+                margin: "18px 0px",
+                fontFamily: "Roboto Condensed",
+                border: "1px solid #00000066",
+              }}
             >
               <ToggleButton value="left" sx={{ flex: 4 }}>
                 ВСЕ ОБЪЕКТЫ
@@ -150,12 +155,19 @@ export const FilterWidget = () => {
             </ToggleButtonGroup>
           </Box>
           <Box component="section" alignItems="center" container>
-            <Grid container columnSpacing={2} sx={{
-              marginTop: {
-                xs: "20px", sm: "0px"
-
-              }, gap: "8px", display: "flex", justifyContent: "space-between"
-            }}>
+            <Grid
+              container
+              columnSpacing={2}
+              sx={{
+                marginTop: {
+                  xs: "20px",
+                  sm: "0px",
+                },
+                gap: "8px",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
               <Grid item size={{ xs: 12, sm: 6, md: 4 }}>
                 <Select
                   value={age}
@@ -163,7 +175,7 @@ export const FilterWidget = () => {
                   displayEmpty
                   sx={{ width: "100%" }}
                   fullWidth
-                  inputProps={{ 'aria-label': 'Without label' }}
+                  inputProps={{ "aria-label": "Without label" }}
                 >
                   <MenuItem value="">
                     <em>Агент</em>
@@ -181,7 +193,7 @@ export const FilterWidget = () => {
                   displayEmpty
                   sx={{ width: "100%" }}
                   fullWidth
-                  inputProps={{ 'aria-label': 'Without label' }}
+                  inputProps={{ "aria-label": "Without label" }}
                 >
                   <MenuItem value="">
                     <em>None</em>
@@ -199,7 +211,7 @@ export const FilterWidget = () => {
                   displayEmpty
                   sx={{ width: "100%" }}
                   fullWidth
-                  inputProps={{ 'aria-label': 'Without label' }}
+                  inputProps={{ "aria-label": "Without label" }}
                 >
                   <MenuItem value="">
                     <em>None</em>
@@ -217,7 +229,7 @@ export const FilterWidget = () => {
                   displayEmpty
                   sx={{ width: "100%" }}
                   fullWidth
-                  inputProps={{ 'aria-label': 'Without label' }}
+                  inputProps={{ "aria-label": "Without label" }}
                 >
                   <MenuItem value="">
                     <em>None</em>
@@ -235,7 +247,7 @@ export const FilterWidget = () => {
                   displayEmpty
                   sx={{ width: "100%" }}
                   fullWidth
-                  inputProps={{ 'aria-label': 'Without label' }}
+                  inputProps={{ "aria-label": "Without label" }}
                 >
                   <MenuItem value="">
                     <em>None</em>
@@ -253,7 +265,7 @@ export const FilterWidget = () => {
                   displayEmpty
                   sx={{ width: "100%" }}
                   fullWidth
-                  inputProps={{ 'aria-label': 'Without label' }}
+                  inputProps={{ "aria-label": "Without label" }}
                 >
                   <MenuItem value="">
                     <em>None</em>
@@ -265,15 +277,40 @@ export const FilterWidget = () => {
               </Grid>
             </Grid>
           </Box>
-          <Grid container spacing={2} sx={{
-
-            width: "100%", marginTop: "18px"
-          }}>
+          <Grid
+            container
+            spacing={2}
+            sx={{
+              width: "100%",
+              marginTop: "18px",
+            }}
+          >
             <Grid item size={{ xs: 12, md: 6 }} sx={{ padding: "25px 0px" }}>
-              <Box component="section" sx={{ padding: "0px 24px", border: '1px solid #00000066', borderRadius: 2, gap: "8px", display: "grid" }}>
-                <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between", alignItems: "center" }}>
+              <Box
+                component="section"
+                sx={{
+                  padding: "0px 24px",
+                  border: "1px solid #00000066",
+                  borderRadius: 2,
+                  gap: "8px",
+                  display: "grid",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
                   <Typography variant="h6">Цена</Typography>
-                  <Typography variant="body1" color="divider" sx={{ margin: "18px 0px" }}>Валюта:
+                  <Typography
+                    variant="body1"
+                    color="divider"
+                    sx={{ margin: "18px 0px" }}
+                  >
+                    Валюта:
                     <ToggleButtonGroup
                       value={currency}
                       exclusive
@@ -281,40 +318,54 @@ export const FilterWidget = () => {
                       onChange={handleCurrency}
                       aria-label="text alignment"
                     >
-                      <ToggleButton value="som" sx={{ border: "#00000000", padding: "5px 10px" }} aria-label="left aligned">
+                      <ToggleButton
+                        value="som"
+                        sx={{ border: "#00000000", padding: "5px 10px" }}
+                        aria-label="left aligned"
+                      >
                         Сом
                       </ToggleButton>
-                      <ToggleButton value="dollar" sx={{ border: "#00000000", padding: "5px 10px" }} aria-label="centered">
+                      <ToggleButton
+                        value="dollar"
+                        sx={{ border: "#00000000", padding: "5px 10px" }}
+                        aria-label="centered"
+                      >
                         Доллар
                       </ToggleButton>
-                    </ToggleButtonGroup></Typography>
-                </Box>
-                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                  <Typography color="primary"
-                    sx={
-                      {
-                        fontSize: { sm: "18px", md: "28px" },
-                        fontWeight: { sm: 400, md: 600 },
-                        fontFamily: "Roboto Condensed"
-                      }
-                    }
-                  >
-                    От {value1[0].toLocaleString('ru-RU')} сом
+                    </ToggleButtonGroup>
                   </Typography>
-
-                  <Typography color="primary" sx={
-                    {
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    color="primary"
+                    sx={{
                       fontSize: { sm: "18px", md: "28px" },
                       fontWeight: { sm: 400, md: 600 },
-                      fontFamily: "Roboto Condensed"
-                    }
-                  }>
-                    До {value1[1].toLocaleString('ru-RU')} сом
+                      fontFamily: "Roboto Condensed",
+                    }}
+                  >
+                    От {value1[0].toLocaleString("ru-RU")} сом
                   </Typography>
 
+                  <Typography
+                    color="primary"
+                    sx={{
+                      fontSize: { sm: "18px", md: "28px" },
+                      fontWeight: { sm: 400, md: 600 },
+                      fontFamily: "Roboto Condensed",
+                    }}
+                  >
+                    До {value1[1].toLocaleString("ru-RU")} сом
+                  </Typography>
                 </Box>
                 <Slider
-                  getAriaLabel={() => 'Minimum distance'}
+                  getAriaLabel={() => "Minimum distance"}
                   value={value1}
                   onChange={handleChangeSlider}
                   valueLabelDisplay="auto"
@@ -327,14 +378,18 @@ export const FilterWidget = () => {
               </Box>
             </Grid>
 
-            <Grid item size={{ xs: 12, md: 6 }} sx={{ gap: "18px", display: "grid" }}>
+            <Grid
+              item
+              size={{ xs: 12, md: 6 }}
+              sx={{ gap: "18px", display: "grid" }}
+            >
               <Select
                 value={age}
                 onChange={handleChange}
                 displayEmpty
                 sx={{ width: "100%" }}
                 fullWidth
-                inputProps={{ 'aria-label': 'Without label' }}
+                inputProps={{ "aria-label": "Without label" }}
               >
                 <MenuItem value="">
                   <em>None</em>
@@ -349,7 +404,7 @@ export const FilterWidget = () => {
                 displayEmpty
                 sx={{ width: "100%" }}
                 fullWidth
-                inputProps={{ 'aria-label': 'Without label' }}
+                inputProps={{ "aria-label": "Without label" }}
               >
                 <MenuItem value="">
                   <em>None</em>
@@ -364,7 +419,7 @@ export const FilterWidget = () => {
                 displayEmpty
                 sx={{ width: "100%" }}
                 fullWidth
-                inputProps={{ 'aria-label': 'Without label' }}
+                inputProps={{ "aria-label": "Without label" }}
               >
                 <MenuItem value="">
                   <em>None</em>
@@ -374,10 +429,16 @@ export const FilterWidget = () => {
                 <MenuItem value={30}>Thirty</MenuItem>
               </Select>
             </Grid>
-
           </Grid>
-          <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", gap: { xs: "5px", sm: "10px" }, marginTop: "18px" }}>
-
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: { xs: "5px", sm: "10px" },
+              marginTop: "18px",
+            }}
+          >
             <Button
               variant="outlined"
               color="primary"
@@ -390,7 +451,7 @@ export const FilterWidget = () => {
                 color: "primary.main",
                 borderColor: "primary.main",
                 textDecoration: "underline",
-                '&:hover': {
+                "&:hover": {
                   backgroundColor: "#f5faff",
                   borderColor: "primary.main",
                   color: "primary.main",
@@ -405,8 +466,9 @@ export const FilterWidget = () => {
               color="primary"
               type="submit"
               sx={{
-                width: "100%", padding: { xs: "10px 2px", sm: "14px 18px" },
-                fontFamily: "Roboto Condensed"
+                width: "100%",
+                padding: { xs: "10px 2px", sm: "14px 18px" },
+                fontFamily: "Roboto Condensed",
               }}
             >
               Применить фильтры
@@ -419,7 +481,9 @@ export const FilterWidget = () => {
         <DialogTitle>Данные фильтра</DialogTitle>
         <DialogContent>
           <div>Возраст: {formData.age?.toString()}</div>
-          <div>Цена: {formData.value1?.[0]} – {formData.value1?.[1]}</div>
+          <div>
+            Цена: {formData.value1?.[0]} – {formData.value1?.[1]}
+          </div>
           <div>Валюта: {formData.currency}</div>
           <div>Тип: {formData.alignment}</div>
         </DialogContent>
