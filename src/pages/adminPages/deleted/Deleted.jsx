d082b8b2a5397e60190ac0f8f0f0c6bb486e1340
Sultanbek02173 <sultan.02173@gmail.com>
2025-08-29 13:23:41 +0600

    fix bugs

--- src/pages/adminPages/deleted/Deleted.jsx
+++ src/pages/adminPages/deleted/Deleted.jsx
@@ -7,100 +7,10 @@ import { useEffect } from "react";
 import { publishedGet } from "../../../app/store/reducers/admin/published/publishedThunk";
 
 export const Deleted = () => {
-  // const data = [
-  //   {
-  //     id: 1,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 2,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 3,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 4,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 5,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 6,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 7,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 8,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 9,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  //   {
-  //     id: 10,
-  //     img: img,
-  //     title: "Готовые квартиры с ремонтом",
-  //     location: "Аламедин-1, ул. Тыныстанова",
-  //     description: "1 комн • 38 м²",
-  //     price: "3 400 000 сом",
-  //     liked: false,
-  //   },
-  // ];
   const dispatch = useDispatch();
   const { list: data } = usePublished();
+
+  console.log(data);
 
   useEffect(() => {
     dispatch(publishedGet({ status: "deleted" }));
